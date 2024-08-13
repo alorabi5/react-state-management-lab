ac7e76c6bdda58161baaa67f4c7b16f4e7fa3e6f
Redha Alaraibi <reda9808@gmail.com>
2024-08-13 09:54:51 +0300

    v1.0

---
+++ components/ZombieFighters.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+
+const ZombieFighters = (props) => {
+
+    
+
+    return (
+      <>
+        <img src={props.zombieFighter.img} alt="" />
+        <h3>Name: {props.zombieFighter.name}</h3>
+        <p>Price: {props.zombieFighter.price}</p>
+        <p>Strength: {props.zombieFighter.strength}</p>
+        <p>Agility: {props.zombieFighter.agility}</p>
+        <button onClick={() => props.handleAddFighter(props.zombieFighter)}>
+          Add
+        </button>
+      </>
+    );
+}
+
+export default ZombieFighters;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,134 @@
 // src/App.jsx
 
+import ZombieFighters from '../components/zombieFighters';
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+
+  const [team, setTeam] = useState([])
+  const [money, setMoney] = useState(100)
+  const [zombieFighters, setZombieFighters] = useState([
+    {
+      name: "Survivor",
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: "https://via.placeholder.com/150/92c952",
+    },
+    {
+      name: "Scavenger",
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: "https://via.placeholder.com/150/771796",
+    },
+    {
+      name: "Shadow",
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: "https://via.placeholder.com/150/24f355",
+    },
+    {
+      name: "Tracker",
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/d32776",
+    },
+    {
+      name: "Sharpshooter",
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: "https://via.placeholder.com/150/1ee8a4",
+    },
+    {
+      name: "Medic",
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: "https://via.placeholder.com/150/66b7d2",
+    },
+    {
+      name: "Engineer",
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: "https://via.placeholder.com/150/56acb2",
+    },
+    {
+      name: "Brawler",
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: "https://via.placeholder.com/150/8985dc",
+    },
+    {
+      name: "Infiltrator",
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: "https://via.placeholder.com/150/392537",
+    },
+    {
+      name: "Leader",
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/602b9e",
+    },
+  ]);
+
+      console.log(team);
+
+  const handleAddFighter = (newTeamFighter) => {
+    console.log(newTeamFighter.price)
+    if (money >= newTeamFighter.price) {
+      setMoney(money - newTeamFighter.price)
+      setTeam([...team, newTeamFighter]);
+    } else {
+      console.log("Not enough money");
+    }
+  };
+
+  
+
+  const zombieFighterList = zombieFighters.map((zombieFighter, idx) => (
+    <li>
+      <ZombieFighters
+        key={idx}
+        zombieFighter={zombieFighter}
+        handleAddFighter={() => handleAddFighter(zombieFighter)}
+      />
+    </li>
+  ));
+
+const ourTeam = team.map((member) => (
+  
+  
+  <li>
+    <img src={member.img} alt="" />
+    <h3>Name: {member.name}</h3>
+    <p>Price: {member.price}</p>
+    <p>Strength: {member.strength}</p>
+    <p>Agility: {member.agility}</p>
+  </li>
+));
+
+
+  return (
+    <>
+      <h2>Money: {money}</h2>
+      <ul>{zombieFighterList}</ul>
+
+    <h1>Team Member</h1>
+    
+    
+      {team.length !== 0? <ul>{ourTeam}</ul> : "No team members.."}
+    </>
+  );
 };
 
 export default App;
